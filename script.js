d1bb8e58252b42fa60de55b85bcd995337502bb7
Bensonw6 <157864218+Bensonw6@users.noreply.github.com>
2026-05-03 17:59:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function handleForm(event, type) {
+  event.preventDefault();
+  alert(type + " form submitted successfully!");
+}
