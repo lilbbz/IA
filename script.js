7b83a890f476004d67a532d799c237da834441e4
Bensonw6 <157864218+Bensonw6@users.noreply.github.com>
2026-05-21 16:40:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ function handleForm(){
     alert("✅ Submitted successfully!");
     return false;
 }
+function gotopage1(){
+    window.location.href="https://youtu.be/dQw4w9WgXcQ?si=TQoXZGKV4HC7e_g_";
+}
