46ec19a703986dce387526bd8aa8daf9c54e5ced
Bensonw6 <157864218+Bensonw6@users.noreply.github.com>
2026-05-27 13:44:35 +0800

    Add files via upload
    No more scroll or zoom + java script and css change

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function handleForm(){
     alert("✅ Submitted successfully!");
     return false;
 }
+
+window.addEventListener('wheel', function(event) {
+  if (event.ctrlKey) {
+    // Prevent the default browser zoom behavior
+    event.preventDefault();
+ }
+}, { passive: false }); // 'passive: false' is required to use preventDefault()
