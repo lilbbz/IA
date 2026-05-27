dd5028ff5c6018bd4ddbb63f7faedf21b445ed52
Bensonw6 <157864218+Bensonw6@users.noreply.github.com>
2026-05-27 13:12:26 +0800

    Add files via upload
    Newest update to file benson

--- script.js
+++ script.js
@@ -5,6 +5,3 @@ function handleForm(){
     alert("✅ Submitted successfully!");
     return false;
 }
-function gotopage1(){
-    window.location.href="https://youtu.be/dQw4w9WgXcQ?si=TQoXZGKV4HC7e_g_";
-}
